67332d0d7b5a5a5baeaf0265355d29ff67b11a90
jberg <jordannealberg@gmail.com>
2018-06-11 16:19:18 -0700

    more eval tests

--- test/mdparser/eval_test.cljs
+++ test/mdparser/eval_test.cljs
@@ -17,4 +17,87 @@
              {"x" -1 "y" 1 "z" 2})))
     (let [f (create-fun (emitter/emit 2 (parser/parse "x = y * z;")))]
       (is (= (js->clj (f (clj->js {:y 1 :z 2})))
-             {"x" 2 "y" 1 "z" 2})))))
+             {"x" 2 "y" 1 "z" 2}))))
+  (testing "tesing conditional operators"
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = y == z;")))]
+      (is (= (js->clj (f (clj->js {:y 1 :z 2})))
+             {"x" 0 "y" 1 "z" 2}))
+      (is (= (js->clj (f (clj->js {:y 1 :z 1})))
+             {"x" 1 "y" 1 "z" 1}))
+      (is (= (js->clj (f (clj->js {:y 1.0000001 :z 1})))
+             {"x" 1 "y" 1.0000001 "z" 1})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = y != z;")))]
+      (is (= (js->clj (f (clj->js {:y 1 :z 2})))
+             {"x" 1 "y" 1 "z" 2}))
+      (is (= (js->clj (f (clj->js {:y 1 :z 1})))
+             {"x" 0 "y" 1 "z" 1}))
+      (is (= (js->clj (f (clj->js {:y 1.0000001 :z 1})))
+             {"x" 0 "y" 1.0000001 "z" 1})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = y < z;")))]
+      (is (= (js->clj (f (clj->js {:y 1 :z 2})))
+             {"x" 1 "y" 1 "z" 2}))
+      (is (= (js->clj (f (clj->js {:y 1 :z 1})))
+             {"x" 0 "y" 1 "z" 1})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = y > z;")))]
+      (is (= (js->clj (f (clj->js {:y 2 :z 1})))
+             {"x" 1 "y" 2 "z" 1}))
+      (is (= (js->clj (f (clj->js {:y 1 :z 1})))
+             {"x" 0 "y" 1 "z" 1})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = y <= z;")))]
+      (is (= (js->clj (f (clj->js {:y 1 :z 2})))
+             {"x" 1 "y" 1 "z" 2}))
+      (is (= (js->clj (f (clj->js {:y 2 :z 1})))
+             {"x" 0 "y" 2 "z" 1}))
+      (is (= (js->clj (f (clj->js {:y 1 :z 1.0000001})))
+             {"x" 1 "y" 1 "z" 1.0000001})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = y >= z;")))]
+      (is (= (js->clj (f (clj->js {:y 2 :z 1})))
+             {"x" 1 "y" 2 "z" 1}))
+      (is (= (js->clj (f (clj->js {:y 1 :z 2})))
+             {"x" 0 "y" 1 "z" 2}))
+      (is (= (js->clj (f (clj->js {:y 1.0000001 :z 1})))
+             {"x" 1 "y" 1.0000001 "z" 1})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = y && z;")))]
+      (is (= (js->clj (f (clj->js {:y 1 :z 1})))
+             {"x" 1 "y" 1 "z" 1}))
+      (is (= (js->clj (f (clj->js {:y 1 :z 0})))
+             {"x" 0 "y" 1 "z" 0}))
+      (is (= (js->clj (f (clj->js {:y 0 :z 0})))
+             {"x" 0 "y" 0 "z" 0})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = y || z;")))]
+      (is (= (js->clj (f (clj->js {:y 1 :z 1})))
+             {"x" 1 "y" 1 "z" 1}))
+      (is (= (js->clj (f (clj->js {:y 1 :z 0})))
+             {"x" 1 "y" 1 "z" 0}))
+      (is (= (js->clj (f (clj->js {:y 0 :z 0})))
+             {"x" 0 "y" 0 "z" 0})))))
+
+(deftest test-functions
+  (testing "testing basic functions"
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = floor(y);")))]
+      (is (= (js->clj (f (clj->js {:y 1})))
+             {"x" 1 "y" 1}))
+      (is (= (js->clj (f (clj->js {:y 1.99999})))
+             {"x" 1 "y" 1.99999})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x = max(y, z);")))]
+      (is (= (js->clj (f (clj->js {:y 1 :z 2})))
+             {"x" 2 "y" 1  "z" 2}))
+      (is (= (js->clj (f (clj->js {:y 2 :z 1})))
+             {"x" 2 "y" 2  "z" 1}))
+      (is (= (js->clj (f (clj->js {:y -2 :z 0})))
+             {"x" 0 "y" -2 "z" 0})))))
+
+(deftest test-assign-op
+  (testing "assign ops"
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x += y;")))]
+      (is (= (js->clj (f (clj->js {:x 0 :y 1})))
+             {"x" 1 "y" 1})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x -= y;")))]
+      (is (= (js->clj (f (clj->js {:x 1 :y 1})))
+             {"x" 0 "y" 1})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x *= y;")))]
+      (is (= (js->clj (f (clj->js {:x 2 :y 3})))
+             {"x" 6 "y" 3})))
+    (let [f (create-fun (emitter/emit 2 (parser/parse "x %= y;")))]
+      (is (= (js->clj (f (clj->js {:x 3 :y 2})))
+             {"x" 1 "y" 2})))))
